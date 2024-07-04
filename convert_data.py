df4375017fd1367d066ebae0540e78bd368357e2
james_a_stephans <130923807+KetsuPami@users.noreply.github.com>
2024-07-04 17:38:20 -0500

    Alter the Python script to scale more effectively, and add documentation

--- convert_data.py
+++ convert_data.py
@@ -1,22 +1,28 @@
 import csv
-
-def process_csv(file_path, dest_file):
-    data_reader = csv.reader(file_path)
-
-    for row in data_reader:
-        if row[0] == "pink morsel":
-            price = float(row[1][1:])
-            quantity = int(row[2])
-            output = str(price * quantity) + "," + row[3] + "," + row[4]
-            print(output, file=dest_file)
+from pathlib import Path
 
 with open("./output_file.csv", "w") as new_file:
+    #Print header for output file.
     print("sales,date,region", file=new_file)
-    with open("./data/daily_sales_data_0.csv") as data_one:
-        process_csv(data_one, new_file)
 
-    with open("./data/daily_sales_data_1.csv") as data_two:
-        process_csv(data_two, new_file)
+    #Get all csv files in the data folder.
+    data_path = Path("./data")
+    csv_files = data_path.glob("*.csv")
 
-    with open("./data/daily_sales_data_2.csv") as data_three:
-        process_csv(data_three, new_file)
+    #Process all csv files into the designated output file.
+    for file in csv_files:
+        with open(file) as file:
+            data_reader = csv.reader(file)
+
+            for row in data_reader:
+                #Only process rows for the pink morsel product. This also skips the header row.
+                if "pink morsel" in row:
+                    #Slice the $ character out of the price column of this row, then convert it into a floating point number.
+                    price = float(row[1][1:])
+                    quantity = int(row[2])
+
+                    #Prepare a new row to go into the output file.
+                    output = str(price * quantity) + "," + row[3] + "," + row[4]
+
+                    #Print the new row into the output file.
+                    print(output, file=new_file)
