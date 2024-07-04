1ff33c649762121f7fbfefb6fa004a3d1fbdd9eb
james_a_stephans <130923807+KetsuPami@users.noreply.github.com>
2024-07-04 18:11:19 -0500

    Shorten graph header, convert file path into a variable

--- display_data.py
+++ display_data.py
@@ -2,19 +2,21 @@ from dash import Dash, html, dcc
 import plotly.express as px
 import pandas as pd
 
+FILE_PATH = "./output_file.csv"
+
 #Initialize Dash app.
 app = Dash(__name__)
 
 #Read the csv file into a pandas DataFrame and sort by date.
-df = pd.read_csv("./output_file.csv")
+df = pd.read_csv(FILE_PATH)
 df.sort_values("date")
 
 #Create the line chart.
-fig = px.line(df, x="date", y="sales", labels={"date": "Date", "sales": "Sale Revenue in USD"})
+fig = px.line(df, x="date", y="sales", labels={"date": "Date", "sales": "Sale Revenue in USD"}, title="Pink Morsel Sales")
 
 #Display the line chart and header in the app.
 app.layout = html.Div(children=[
-    html.H1(children='Graph of By-Day Sale Revenue from February 6th, 2018 to February 14th, 2022'),
+    html.H1(children='Graph of Pink Morsel Sale Revenue'),
 
     dcc.Graph(
         id='sales-graph',
@@ -24,4 +26,4 @@ app.layout = html.Div(children=[
 
 #Run the Dash app if this file was called by Python directly.
 if __name__ == '__main__':
-    app.run()
+    app.run_server()
