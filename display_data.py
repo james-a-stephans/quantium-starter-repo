a2ce6b077258eab8db273a37feb9f7656a809419
james_a_stephans <130923807+KetsuPami@users.noreply.github.com>
2024-07-04 19:01:22 -0500

    Add styling and region control to the graph.

--- display_data.py
+++ display_data.py
@@ -1,28 +1,60 @@
-from dash import Dash, html, dcc
+from dash import Dash, html, dcc, Input, Output, callback
 import plotly.express as px
 import pandas as pd
 
-FILE_PATH = "./output_file.csv"
+FILE_PATH = './output_file.csv'
 
 #Initialize Dash app.
 app = Dash(__name__)
 
 #Read the csv file into a pandas DataFrame and sort by date.
 df = pd.read_csv(FILE_PATH)
-df.sort_values("date")
+df.sort_values('date')
 
-#Create the line chart.
-fig = px.line(df, x="date", y="sales", labels={"date": "Date", "sales": "Sale Revenue in USD"}, title="Pink Morsel Sales")
-
-#Display the line chart and header in the app.
+#Establish the layout for the graph display, including the header and radio buttons.
 app.layout = html.Div(children=[
     html.H1(children='Graph of Pink Morsel Sale Revenue'),
 
     dcc.Graph(
         id='sales-graph',
-        figure=fig
-    )
-])
+        className='graph-sales'
+    ),
+
+    dcc.RadioItems(['All', 'North', 'South', 'East', 'West'], 'All', inline=True, id="region", className="options"),
+]),
+
+
+#Determine which region to display sales data from.
+@callback(
+    Output(component_id='sales-graph', component_property='figure'),
+    Input(component_id='region', component_property='value')
+)
+def change_region(input_value):
+    if(input_value == 'All'):
+        return px.line(
+            df, x='date', y='sales', labels={'date': 'Date', 'sales': 'Sale Revenue in USD'}, title='Total Pink Morsel Sales'
+        )
+    elif(input_value == 'North'):
+        return px.line(
+            df.loc[df['region'] == 'north'], x='date', y='sales', labels={'date': 'Date', 'sales': 'Sale Revenue in USD'},
+            title='Pink Morsel Sales in the North Region'
+        )
+    elif(input_value == 'South'):
+        return px.line(
+            df.loc[df['region'] == 'south'], x='date', y='sales', labels={'date': 'Date', 'sales': 'Sale Revenue in USD'},
+            title='Pink Morsel Sales in the South Region'
+        )
+    elif(input_value == 'East'):
+        return px.line(
+            df.loc[df['region'] == 'east'], x='date', y='sales', labels={'date': 'Date', 'sales': 'Sale Revenue in USD'},
+            title='Pink Morsel Sales in the East Region'
+        )
+    elif(input_value == 'West'):
+        return px.line(
+            df.loc[df['region'] == 'west'], x='date', y='sales', labels={'date': 'Date', 'sales': 'Sale Revenue in USD'},
+            title='Pink Morsel Sales in the East Region'
+        )
+
 
 #Run the Dash app if this file was called by Python directly.
 if __name__ == '__main__':
