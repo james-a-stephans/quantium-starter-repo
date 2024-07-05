86fe7841321c1df4c54a48dfa679272850cdbde7
james_a_stephans <130923807+KetsuPami@users.noreply.github.com>
2024-07-04 20:04:09 -0500

    Update tests to no longer require deployment.

--- test_display_data_layout.py
+++ test_display_data_layout.py
@@ -1,15 +1,13 @@
-import dash
-from dash import html
-from selenium.webdriver.common.by import By
+from display_data import app
 
-def test_001_header(dash_br):
-    dash_br.server_url = "http://127.0.0.1:8050"
-    dash_br.driver.find_elements(By.ID, "header")
+def test_001_header(dash_duo):
+    dash_duo.start_server(app)
+    dash_duo.wait_for_element('#header', timeout=8)
 
-def test_002_graph(dash_br):
-    dash_br.server_url = "http://127.0.0.1:8050"
-    dash_br.driver.find_elements(By.ID, "sales-graph")
+def test_002_graph(dash_duo):
+    dash_duo.start_server(app)
+    dash_duo.wait_for_element('#sales-graph', timeout=8)
 
-def test_003_region(dash_br):
-    dash_br.server_url = "http://127.0.0.1:8050"
-    dash_br.driver.find_elements(By.ID, "region")
+def test_003_region(dash_duo):
+    dash_duo.start_server(app)
+    dash_duo.wait_for_element('#region', timeout=8)
